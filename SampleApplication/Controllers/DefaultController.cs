42d9804bd2617d88c58caa4ffe840836d35c7356
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-11-15 15:47:29 +0300

    Sending and Capturing Tuple Objects is now available

---
+++ SampleApplication/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleApplication.Models;
+
+namespace SampleApplication.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            var tuple = (new Product(), new User());
+            return View(tuple);
+        }
+        [HttpPost]
+        public IActionResult Index([Bind(Prefix ="Item1")]Product product, [Bind(Prefix = "Item2")]User user)
+        {
+            return View();
+        }
+    }
+}
