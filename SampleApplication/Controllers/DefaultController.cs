b37d43002e092bf6550dddf46be4be9bc3d7ab73
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-11-16 22:33:09 +0300

    Validation of Data Coming from User Validations were used

--- SampleApplication/Controllers/DefaultController.cs
+++ SampleApplication/Controllers/DefaultController.cs
@@ -11,8 +11,23 @@ namespace SampleApplication.Controllers
             return View(tuple);
         }
         [HttpPost]
-        public IActionResult Index([Bind(Prefix ="Item1")]Product product, [Bind(Prefix = "Item2")]User user)
+        public IActionResult Index([Bind(Prefix = "Item1")] Product product, [Bind(Prefix = "Item2")] User user)
         {
+            return View();
+        }
+
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.ErrorMessage = ModelState.Values.FirstOrDefault(x => x.ValidationState == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid).Errors[0].ErrorMessage;
+                return View(product);
+            }
             return View();
         }
     }
--- SampleApplication/Models/Product.cs
+++ SampleApplication/Models/Product.cs
@@ -1,9 +1,16 @@
-﻿namespace SampleApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleApplication.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Lütfen Name kısmını giriniz!!")]
+        [StringLength(100,ErrorMessage ="en fazla 100 karakter olmalı.")]
         public string Name { get; set; }
         public string Description { get; set; }
+
+        [EmailAddress(ErrorMessage ="Lütfen email addresini doğru giriniz!!")]
+        public string Email { get; set; }
     }
 }
