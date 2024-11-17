fc69bc5b8270365ac68b21733f67af25d5c4c08c
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-11-17 16:36:40 +0300

    ViewComponent Created used

---
+++ SampleApplication/ViewComponents/PersonellerViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApplication.ViewComponents
+{
+    public class PersonellerViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
